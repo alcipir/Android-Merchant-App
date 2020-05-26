beebc6d0b22296858223060debd9ec9e88235e8e
Andre Cabrera <andrecabrera@protonmail.ch>
2020-05-26 02:40:20 -0300

    Auto-convert to crypto screen

--- app/src/main/java/com/bitcoin/merchant/app/screens/SettlementFragment.kt
+++ app/src/main/java/com/bitcoin/merchant/app/screens/SettlementFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Switch
 import com.bitcoin.merchant.app.R
 import com.bitcoin.merchant.app.screens.features.ToolbarAwareFragment
+import com.google.android.material.textfield.TextInputEditText
 
 
 class SettlementFragment : ToolbarAwareFragment() {
@@ -18,7 +22,22 @@ class SettlementFragment : ToolbarAwareFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_settlement, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_settlement, container, false)
+        val addressInput : TextInputEditText = rootView.findViewById(R.id.settlement_address_input)
+        addressInput.isEnabled = false
+
+        val settlementSpinnerItems : Array<String> = arrayOf("USDH", "USDT", "USDC")
+        val settlementSpinnerItemsAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, settlementSpinnerItems)
+        val settlementSpinner: Spinner = rootView.findViewById(R.id.settlement_spinner)
+        settlementSpinner.adapter = settlementSpinnerItemsAdapter
+        settlementSpinner.isEnabled = false
+
+        val settlementSwitch : Switch = rootView.findViewById(R.id.settlement_switch)
+        settlementSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+                addressInput.isEnabled = isChecked
+                settlementSpinner.isEnabled = isChecked
+        }
+        return rootView;
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
